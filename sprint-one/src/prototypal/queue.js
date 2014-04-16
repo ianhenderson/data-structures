d506e112359380ccc09a41810e2e1f30bd406e6c
Hack Reactor Students <ianwhen@gmail.com>
2014-04-16 16:05:25 -0700

    Finished prototypal style.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,25 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  var instance = Object.create(queueMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.index = 0;
+  return instance;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value){
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+queueMethods.dequeue = function(){
+  var keyLength = Object.keys(this.storage).length;
+  var output = this.storage[this.index-keyLength];
+  delete this.storage[this.index-keyLength];
+  return output;
+};
+
+queueMethods.size = function(){
+  return Object.keys(this.storage).length;
+};
