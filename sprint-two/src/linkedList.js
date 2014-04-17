3c1a76b292223df447022e39652acdf81600642c
Hack Reactor Students <ianwhen@gmail.com>
2014-04-17 11:38:59 -0700

    Implemented doubly-linked lists and finshed associated extra credit.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,21 +4,54 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+    if(list.head === null){
+      list.head = node;
+      list.tail = node;
+    } else{
+      list.tail.next = node;
+      node.previous =  list.tail;
+      list.tail = node;
+    }
+  };
+
+  list.addToHead = function(value){
+    var node = makeNode(value);
+    if(list.head === null){
+      list.head = node;
+      list.tail = node;
+    } else{
+      list.head.previous = node;
+      node.next =  list.head;
+      list.head = node;
+    }
   };
 
   list.removeHead = function(){
+    list.head = list.head.next;
+    list.head.previous = null;
   };
 
   list.contains = function(target, node){
+    node = node || list.head;
+    if (node.value === target){
+      return true;
+    } else if (node.next === null){
+      return false;
+    } else{
+      return list.contains(target, node.next);
+    }
+  };
+
+  var makeNode = function(value){
+    var node = {};
+    node.value = value;
+    node.next = null;
+    node.previous = null;
+
+    return node;
   };
 
   return list;
 };
 
-var makeNode = function(value){
-  var node = {};
-  node.value = value;
-  node.next = null;
-
-  return node;
-};
