9b7260b088704cb63d3da42be8fbb9c876734df8
Hack Reactor Students <ianwhen@gmail.com>
2014-04-18 15:40:12 -0700

    Finished graph problem.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,20 +1,56 @@
-var Graph = function(){
+var Graph = function() {
+  this.nodes = {};
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  var keys = Object.keys(this.nodes);
+  if (keys.length === 1) {
+    toNode = keys[0];
+  }
+
+  this.nodes[newNode] = {};
+  this.nodes[newNode].edges = {};
+  if (toNode) {
+    this.addEdge(newNode, toNode);
+  }
 };
 
 Graph.prototype.contains = function(node){
+  return this.nodes[node] !== undefined ? true: false;
 };
 
 Graph.prototype.removeNode = function(node){
+  //debugger;
+  var edges = this.nodes[node].edges;
+  var keys = Object.keys(edges);
+  if (keys.length === 0) {
+    delete this.nodes[node];
+  } else {
+    for (var key in edges) {
+      this.removeEdge(node, key);
+    }
+  }
+
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+  //debugger;
+  return this.nodes[fromNode].edges[toNode] !== undefined;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.nodes[fromNode].edges[toNode] = true;
+  this.nodes[toNode].edges[fromNode] = true;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  //debugger;
+  delete this.nodes[fromNode].edges[toNode];
+  delete this.nodes[toNode].edges[fromNode];
+  if (Object.keys(this.nodes[fromNode].edges).length === 0) {
+    delete this.nodes[fromNode];
+  }
+  if (Object.keys(this.nodes[toNode].edges).length === 0) {
+    delete this.nodes[toNode];
+  }
 };
