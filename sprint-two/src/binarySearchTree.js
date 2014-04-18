da8dbc3e3b7a1af2e4c4ffabd3af65f5aae124e6
Hack Reactor Students <ianwhen@gmail.com>
2014-04-17 23:13:14 -0700

    still working on binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,9 +1,27 @@
 var makeBinarySearchTree = function(value){
+  var result = Object.create(BSTMethods);
+  result.value = value;
+  result.left = null;
+  result.right = null;
+  return result;
 };
 
 var BSTMethods = {};
 
-BSTMethods.insert = function(value){};
+BSTMethods.insert = function(value){
+  debugger;
+  var node =  makeBinarySearchTree(value);
+  // path = path || this;
+  var dir = value > this.value ? this.right : this.left;
+
+  if (dir !== null) {
+    dir.insert(value);
+  } else {
+    dir = node;
+  }
+
+  // if (node.value === this.value)
+};
 
 BSTMethods.contains = function(value){};
 
