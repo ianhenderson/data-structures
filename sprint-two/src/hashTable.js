0fcb4785ec32e955699b6ba13806543705b64eb4
Hack Reactor Students <ianwhen@gmail.com>
2014-04-18 20:11:17 -0700

    partially implemented hashTable.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,16 +1,34 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+  this._size = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var i;
+  this._size++;
+  debugger;
+  if (this._size >= 7){
+    this._limit = 2 * this._limit;
+    console.log("limit", this._limit);
+    this._tmpStorage = makeLimitedArray(this._limit);
+    this._storage.each(function(value, index){
+        i = getIndexBelowMaxForKey(value, this._limit);
+        this._tmpStorage.set(i,value);
+    });
+    this._storage = this._tmpStorage;
+  }
+  i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i,v);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
+  this._size--;
+  this.insert(k, null);
 };
 
